77da51f99fc6c99d3556234c259af0da65645b1b
muhammadzeeshan22 <muhammadzeeshan72023@gmail.com>
2025-04-06 20:02:22 +0500

    Add files via upload

---
+++ 02app.py
@@ -0,0 +1,48 @@
+# connected sir aneeq sir aleem
+
+name: str = "Muhammad zeeshan"
+age: int = 20
+is_active: bool = True
+print(name)
+
+# yahan men variable ko override kr rha hn
+name = "sir aneeq"
+print(name)
+
+
+#                 Data Types
+# Integers (int)                (Digits 0 to 9 etc)
+# String (str)                  (Hellow world etc)
+# Float (Decimal value)         (2.5 , 9.0 , 8.0 , 3.99) 
+# Boolean                       (True/False)
+# None                          (a = None)
+# while loops
+# For loop                          
+# list                              [mutable] Multiple value
+# Tupple                            (immutable)
+# Dictionaries                      (Indux)
+# Functions                         block {} code
+
+
+# Example Variables
+
+# name = "Muhammad zeeshan"
+# digits = 50
+# price = 3.99
+# old = False
+# a = None
+# print(type(name))              # (str) 
+# print(type(digits))            # (int)
+# print(type(price))             # (float)
+# print(type(old))               # (Boolean)
+# print(type(a))                 # (None Type)
+
+
+# Variables  Assignment
+
+name = "Muhammad zeeshan"
+age = 20
+price = 3.99
+print(name)
+print(age)
+print(price)
