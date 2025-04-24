36afbb2b4a632776576d2902ae835998efe5fc02
Muhammad Zeeshan <muhammadzeeshan72023@gmail.com>
2025-04-24 10:23:56 +0500

    Add files via upload

---
+++ 05app.py
@@ -0,0 +1,104 @@
+#  condition -> True/False
+# if False:
+#   print("condition is true")
+# else:
+#   print("condition is false")
+
+# comparison operators -> True / False
+# num:int = 10
+# print(num < 10) 
+#  num > 0 = True
+# print(num > 0) # True
+# if num > 0: # agr num variable he wo zero bara
+#     print("number is positive")
+# else:
+#     print("number is negative")    
+ 
+# num:int = 100
+#     True    and    False
+# print(num > 0 and num < 100)
+# if num > 0 and num < 100: # agr num variable he wo zero bara
+#     print("number is positive")
+# else:
+#     print("number is negative")    
+ 
+# name:str = "Sir Ali"
+
+# if name == "Sir Sohaib" :
+#     print("welcome Sir Sohaib") # indentation
+# else:
+#     print("App Kon")  
+
+# print("Kesse hen Aap")
+
+# snake case
+# time_of_day: str = "abc"
+
+# if time_of_day == "morning":
+#     print("Good Morning")
+# elif time_of_day == "afternoon":
+#     print("Good AfterNoon") 
+# elif time_of_day == "evening":
+#     print("Good Evening")        
+# else:
+#     print("Good Night")
+
+
+# string
+#integer
+# boolean
+# list
+# name = "Sir Ali"
+#                     0        ,    1       ,   2
+# names: list[str] = ["Sir Aneeq","Sir Sohaib","Sir Sami"]
+#                     0,1,2,3,4 
+
+# print(names)
+# print(names[1])
+# print(numbers[4])
+# numbers[0] # -> 1
+# if numbers[0] > 1:
+
+# print(range(5))
+# res = range(1,5)
+# starting point
+# condition
+# increment
+
+# print(numbers)
+# number2 = list(range(1,5))
+# print(number2)
+# print(list[range(1,5)])
+# numbers: list[int] = [1,2,3,4,5]
+
+
+
+# for loop
+# iterables
+# numbers: list[int] = [1,2,3,4,5] # iterable
+# print(numbers[0])
+# print(numbers[1])
+# print(numbers[2])
+# print(numbers[3])
+# print(numbers[4])
+
+# for variable in iterable:
+# num = numbers[0] -> 1
+# num = numbers[1] -> 2
+# for n in numbers:
+#   print("n is",n)
+
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print("Aam Zindagi")
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+
+
+print("Mintos Zindagi")
+for num in numbers:
+    print(num)
